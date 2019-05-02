935ee3e3401457a766eebf53472f57298016f01d
Michal Czyzewski <czy.mich@gmail.com>
2019-05-02 13:16:14 +0200

    Initial commit

---
+++ src/pl/mczyzewski/Rectangle.java
@@ -0,0 +1,14 @@
+package pl.mczyzewski;
+
+public class Rectangle
+{
+
+    private double width;
+    private double heigth;
+
+    public Rectangle(double width, double heigth)
+    {
+        this.width = width;
+        this.heigth = heigth;
+    }
+}
